c7eabedee5cdc070c5b2733fe0287b6771dfef92
JuCarvalho1 <logonrmlocal@fiap.com.br>
2024-04-04 20:26:36 -0300

    Complementacao documentacao Classe Calculadora

--- PastaFontesProjeto/ExercicioExtraJUNITSolucao/ProjetoElevador/src/FontesAppElevador/Calculadora.java
+++ PastaFontesProjeto/ExercicioExtraJUNITSolucao/ProjetoElevador/src/FontesAppElevador/Calculadora.java
@@ -1,6 +1,8 @@
 package FontesAppElevador;
 
 //Classe de entidade - Calculadora aritmética de apoio para estimar a carga do elevador
+//Soma e subtrai carga
+
 public class Calculadora {
 
 	//Atributo de resultado de cálculo
